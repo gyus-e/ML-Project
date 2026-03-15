e795978ef24e135577abcad48453997d70dd8f82
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2026-03-15 22:19:13 +0100

    check mean and std are consistent after train-val split with all seeds

--- scripts/mnist_analysis.py
+++ scripts/mnist_analysis.py
@@ -1,6 +1,11 @@
 import torch
+from torch.utils.data import Subset
 from torchvision.datasets import MNIST
 from torchvision.transforms import ToTensor
+from sklearn.model_selection import train_test_split
+
+
+RANDOM_SEEDS = [43, 689, 5093]
 
 DATA_DIR = "data"
 
@@ -8,7 +13,7 @@ FULL_TRAINING_DATA = MNIST(
     root=DATA_DIR,
     train=True,
     download=True,
-    transform=ToTensor(),
+    transform=ToTensor(),  # applica giá min-max normalization
 )
 
 TEST_DATA = MNIST(
@@ -18,11 +23,25 @@ TEST_DATA = MNIST(
     transform=ToTensor(),
 )
 
-x = torch.cat([FULL_TRAINING_DATA[i][0] for i in range(len(FULL_TRAINING_DATA))], dim=0)
-img = FULL_TRAINING_DATA[0][0]
+for seed in RANDOM_SEEDS:
+    train_idx, val_idx = train_test_split(
+                range(len(FULL_TRAINING_DATA)),
+                test_size=0.2,
+                train_size=0.8,
+                random_state=seed,
+                shuffle=True,
+                stratify=FULL_TRAINING_DATA.targets.numpy(),
+            )
 
-print(
-    f"""size: {img.size()}
-      mean: {x.mean().item()}
-      std: {x.std().item()}"""
-)
+    training_data = Subset(FULL_TRAINING_DATA, train_idx)
+    validation_data = Subset(FULL_TRAINING_DATA, val_idx)
+
+    images = torch.cat([X for (X, y) in training_data], dim=0)
+    val_images = torch.cat([X for (X, y) in validation_data], dim=0)
+
+    print(
+        f"""train mean: {images.mean().item()}
+        train std: {images.std().item()}
+        val mean: {val_images.mean().item()}
+        val std: {val_images.std().item()}"""
+    )
